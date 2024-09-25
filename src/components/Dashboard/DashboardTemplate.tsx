1ac7af8f7ff5d9ef3915d591348857e7231edcc7
Milo Bedini <milo.bedini@frw.co.uk>
2024-09-25 23:35:34 +0100

    prevents no server full crash

--- src/components/Dashboard/DashboardTemplate.tsx
+++ src/components/Dashboard/DashboardTemplate.tsx
@@ -8,7 +8,6 @@ function DashboardTemplate() {
   const { data, isError, isLoading } = useMyTaskCompletionsQuery({});
 
   if (isError || isLoading || !data) {
-    console.log('rendering error states');
     return <ErrorStates apiError={isError} apiLoading={isLoading} noContent={!data} />;
   }
 
